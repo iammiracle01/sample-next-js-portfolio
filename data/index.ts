d065f02f88539cb8e4c34143dd102e60d3eea198
Miracle <mbo_seyifash@yahoo.com>
2024-07-10 17:38:41 +0400

    "Updated navItems className and added nodemailer dependency"

--- data/index.ts
+++ data/index.ts
@@ -215,7 +215,7 @@ export const navItems = [
       id: 2,
       title: "Mobile App Dev - JSM Tech",
       desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
+      className: "md:col-span-2",
       thumbnail: "/exp2.svg",
     },
     {
